33eb55d692b0c091952b6aa0abd27e4ed7e1ec84
Arif Uddin <arifunctg@gmail.com>
2020-06-13 11:58:27 +0600

    icon file updaterd

--- src/CloseIcon.jsx
+++ src/CloseIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function CloseIcon() {
   return (
-    <svg className="msl-close-icn">
-      <line x1="0" y1="0" x2="15" y2="15" />
-      <line x1="0" y1="15" x2="15" y2="0" />
+    <svg className='msl-close-icn'>
+      <line x1='0' y1='0' x2='15' y2='15' />
+      <line x1='0' y1='15' x2='15' y2='0' />
     </svg>
   )
 }
--- src/DownIcon.jsx
+++ src/DownIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function DownIcon() {
   return (
-    <svg className="msl-arrow-icn">
-      <line x1="10" y1="17" x2="2" y2="8" />
-      <line x1="18" y1="8" x2="10" y2="17" />
+    <svg className='msl-arrow-icn'>
+      <line x1='10' y1='17' x2='2' y2='8' />
+      <line x1='18' y1='8' x2='10' y2='17' />
     </svg>
   )
 }
